483b6c10fd7f7840d00449d9e8013491f1568e9a
williampariwat <william.pariwat@gmail.com>
2021-01-11 02:21:54 +0700

    Fix all the bugs and styles

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 
+// var email = document.getElementById("email");
+// document.addEventListener("keypress",)
+// document.getElementById("email").addEventListener("mouseout", validate);
 
+document.getElementById("email").addEventListener("blur",validate);
+document.getElementById("email").addEventListener("keypress",validate);
+
+
+//   document.addEventListener('keypress', advance);
 
   function validate() {
-        var email = document.Validation.email.value();
         //Took this regex from https://regexr.com/3e48o
-        var regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-        
-        // email = document.getElementById("input").nodeValue;
-        if(!regex.test(email)){
-           document.getElementById('placeholder').innerHTML = "A valid email is required";
-           email.focus();
-           return false;
+        var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var text = document.getElementById("email").value;
+        if(regex.test(text)){
+           document.getElementById("email").style.border = "2px solid rgb(149, 194, 246)";
+           document.getElementById('error').innerHTML = "";
+           document.getElementById("sub").addEventListener("click",subs);
         }
         else{
-            return true;
+            document.getElementById('error').innerHTML = "A valid email is required";
+            document.getElementById("email").style.border = "2px solid rgb(246, 162, 149)";
+            email.focus();
+
         }
     }
+
+    function subs(){
+        var newHtml = '<div class =card id = "box" style =" \
+        font-family: Arial, Helvetica, sans-serif;\
+        width: 330px;\
+        margin-left: 5px;\
+        padding-right: 5px;\
+        padding: 25px ;\
+        background-color: rgb(208, 244, 213);\
+        line-height: 10px;\
+        margin-top: 0px;\
+        border: 1px solid rgb(224, 224, 224);">\
+        <b style = "color: rgb(28, 96, 20);">Thank You</b>\
+        <p style = "color: rgb(28, 96, 20);">Your subscription was a success!</p>\
+        </div>'
+        document.getElementById("box").innerHTML = newHtml;
+    }
+
+
